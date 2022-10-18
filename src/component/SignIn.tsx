8a9422053c004f9d8829a0ac0606c370d87efd8a
Emil Lau <r09343018@ntu.edu.tw>
2022-10-18 22:19:06 +0800

    Complete sign in page ui

--- src/component/SignIn.tsx
+++ src/component/SignIn.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
-
 import { useNavigate } from "react-router-dom";
-
 import { superbase } from "../utils/client";
+import github from "../img/github.svg";
 
 const App = () => {
   useEffect(() => {
@@ -45,10 +44,30 @@ const App = () => {
   }
 
   return (
-    <>
-      <h1>Hello, please sign in.</h1>
-      <button onClick={signInWithGitHub}>Sign In</button>
-    </>
+    <section className="flex h-[100vh] w-[100vw] flex-col items-center justify-center bg-[#f9f9f9]">
+      <img
+        src={github}
+        className="mb-[24px] h-[48px] w-[48px] cursor-pointer"
+        alt=""
+      ></img>
+      <span className="text-[24px] font-bold text-black">
+        Sign in to GitHub
+      </span>
+      <div className="mt-[16px] flex h-[220px] w-[308px] flex-col items-center border-[1px] border-solid border-[#e4e8ea] bg-white">
+        <span className="mt-[16px] w-[85%] text-center text-[16px] font-semibold">
+          Welcome to Github Issue Remote Control
+        </span>
+        <span className="mt-[32px] w-[85%] text-center text-[14px] text-[grey]">
+          Sign in to choose your repository and have fun.
+        </span>
+        <div
+          onClick={signInWithGitHub}
+          className="mt-[28px] flex h-[32px] w-[240px] cursor-pointer items-center justify-center rounded-md bg-[#2fa455]"
+        >
+          <span className="text-[14px] text-white">Sign in</span>
+        </div>
+      </div>
+    </section>
   );
 };
 
