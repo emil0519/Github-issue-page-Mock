d45102f5fb17070b0170d9e5d34fe1a6ea521da2
Emil <emillau@nine-yi.com>
2024-01-27 16:54:49 +0800

    Refactor: Restructure App & index.tsx to modern React format

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import './index.css'
+
+import { FC } from 'react'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Error from './component/Error/Error'
+import IssueList from './component/IssueList/App'
+import Label from './component/LabelList/Label'
+import NewIssue from './component/NewIssue'
+import RepoList from './pages/RepoList'
+import SignIn from './pages/SignIn'
+import IssuePage from './IssuePage/IssuePage'
+import reportWebVitals from './reportWebVitals'
+import ErrorBoundary from './ErrorBoundary'
+import { store } from './state/store'
+
+const App: FC = () => {
+    return (
+        <ErrorBoundary fallback="Error happened">
+            <BrowserRouter>
+                <Provider store={store}>
+                    <Routes>
+                        <Route path="/" element={<SignIn />}></Route>
+                        <Route path="/Repo" element={<RepoList />}></Route>
+                        <Route path="/App" element={<IssueList />}></Route>
+                        <Route path="/Label" element={<Label />}></Route>
+                        <Route path="/NewIssue" element={<NewIssue />}></Route>
+                        <Route
+                            path="/IssuePage"
+                            element={<IssuePage />}
+                        ></Route>
+                        <Route path="*" element={<Error />}></Route>
+                    </Routes>
+                </Provider>
+            </BrowserRouter>
+        </ErrorBoundary>
+    )
+}
+
+export default App
+
+reportWebVitals()
--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,9 @@
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Error from "./component/Error/Error";
-import App from "./component/IssueList/App";
-import Label from "./component/LabelList/Label";
-import NewIssue from "./component/NewIssue";
-import RepoList from "./pages/RepoList";
-import SignIn from "./pages/SignIn";
-import "./index.css";
-import IssuePage from "./IssuePage/IssuePage";
-import reportWebVitals from "./reportWebVitals";
-import { store } from "./state/store";
-import ErrorBoundary from "./ErrorBoundary";
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+import { render } from "react-dom";
 
-root.render(
-  <ErrorBoundary fallback="Error happened">
-  <BrowserRouter>
-    <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<SignIn />}></Route>
-        <Route path="/Repo" element={<RepoList />}></Route>
-        <Route path="/App" element={<App />}></Route>
-        <Route path="/Label" element={<Label />}></Route>
-        <Route path="/NewIssue" element={<NewIssue />}></Route>
-        <Route path="/IssuePage" element={<IssuePage />}></Route>
-        <Route path="*" element={<Error />}></Route>
-      </Routes>
-    </Provider>
-  </BrowserRouter>
-  </ErrorBoundary>
-);
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
 
 reportWebVitals();
